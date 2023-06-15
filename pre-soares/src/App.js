91b5bacb742f8b8c11d8b3cc11dfbcfc72da04c1
Byasoares18 <105671476+Byasoares18@users.noreply.github.com>
2023-06-15 17:15:39 -0300

    tercera

--- pre-soares/src/App.js
+++ pre-soares/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer gretting={'Platas de qualidad'}/>
+      <ItemListContainer gretting={'Bienvenido a mi tienda'}/>
       <CartWidget/>
     </div>
   );
--- pre-soares/src/components/CartWidget/CartWidget.jsx
+++ pre-soares/src/components/CartWidget/CartWidget.jsx
@@ -1,8 +1,18 @@
 import cart from "./assets/cart.png"
 
+const styles = {
+    img: {
+    heigth: "2rem",
+    width:"auto",
+
+    },
+}
+
 export const CartWidget = () =>(
     <>
-    <img src={cart} alt="CartWidget" />
+    <img src={cart} style={styles.img} alt="CartWidget" />
     <span>0</span>
     </>
-)
+)     
+
+export default CartWidget
