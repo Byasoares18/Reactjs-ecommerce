d6e6b6b00b7f8b26cb7b76e4951fbad31f2dd6fc
Byasoares18 <105671476+Byasoares18@users.noreply.github.com>
2023-07-05 14:25:02 -0300

    edit

--- pre-soares/src/App.js
+++ pre-soares/src/App.js
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemLitContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <ItemListContainer gretting={'Bienvenido a mi tienda'}/>
-      
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('cantidad agregada', quantity)} />
     </div>
   );
 }
